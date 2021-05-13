18676b7b4492e5a23f6d269ea0f646e550c48e04
Gourimmenon <83968970+Gourimmenon@users.noreply.github.com>
2021-05-13 01:55:49 -0700

    Create day3b.c

---
+++ day3b.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    float f;
+    float c;
+    scanf("%f",&f);
+    c = (f-32)*5/9;
+    printf("%.2f",c);
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
