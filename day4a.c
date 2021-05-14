33fc5a9ed892e7eab9456f290cff55a5f0d9e581
Gourimmenon <83968970+Gourimmenon@users.noreply.github.com>
2021-05-14 04:29:05 -0700

    Create day4a.c

---
+++ day4a.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int a;
+  
+    scanf("%d",&a);
+
+    if(a%400==0)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        if (a%4==0)
+        { 
+            if (a%100==0)
+            {
+                printf("No");  
+             }
+            else 
+            {
+                printf("Yes");
+            }
+          
+        }
+        else
+        {
+         printf("No");  
+        }
+    }
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
